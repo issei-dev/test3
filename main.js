4f818bef20141afffee7b7e6c8b6d6314d4a7b63
issei-dev <isseitogasgi.biz@gmail.com>
2025-09-06 21:06:49 +0900

    Update main.js

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     const BOSS_MASTER_DATA = {
-        1: { name: "ゴブリン", image: "531864-backgroundImage1.png", maxHp: 1000, rewardPoints: 500 },
+        1: { name: "ゴブリン", image: "images-002c.png", maxHp: 1000, rewardPoints: 500 },
         2: { name: "巨大スライム", image: "images/boss_slime.png", maxHp: 2500, rewardPoints: 1000 },
         3: { name: "オーガ", image: "images/boss_ogre.png", maxHp: 5000, rewardPoints: 2000 },
     };
